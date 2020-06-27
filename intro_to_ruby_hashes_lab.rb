eb3ad8f0fdd51ebabe613e58628ec33e40cb5467
zachary mines <zmines@wesleyan.edu>
2020-06-27 21:38:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ def monopoly_with_second_tier
 	monopoly[:railroads] = {}
 	monopoly[:railroads][:pieces] = 4
 	monopoly[:railroads][:names] = {}
+	monopoly[:railroads][:rent_in_dollars] = {}
 	monopoly
 end
 

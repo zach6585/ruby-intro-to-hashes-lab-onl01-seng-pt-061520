8eefeb80919710291dc4fa49f282afee18325818
zachary mines <zmines@wesleyan.edu>
2020-06-27 21:42:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,8 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:rent_in_dollars] = {}
 	monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
 	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
-	monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 100
+	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
+	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 50
 	monopoly
 
 end

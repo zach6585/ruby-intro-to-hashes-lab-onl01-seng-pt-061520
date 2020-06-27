ca446a44123f50ec71f644bd5f68e1fe371564c1
zachary mines <zmines@wesleyan.edu>
2020-06-27 21:43:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,6 +34,7 @@ def monopoly_with_third_tier
 	monopoly[:railroads][:rent_in_dollars][:three_pieces_owned] = 100
 	monopoly[:railroads][:rent_in_dollars][:four_pieces_owned] = 200
 	monopoly[:railroads][:names][:reading_railroad] = {}
+	monopoly[:railroads][:names][:pennsylvania_railroad] = {}
 	monopoly
 
 end
